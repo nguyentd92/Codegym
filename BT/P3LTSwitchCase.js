b63d137424ae425d03fdc8de15b22ba207a6baf9
nguyentd92 <45224334+nguyentd92@users.noreply.github.com>
2019-01-08 11:29:30 +0700

    Create P3LTSwitchCase.js

---
+++ BT/P3LTSwitchCase.js
@@ -0,0 +1,30 @@
+// Bai 1
+Switch(browser) {
+    case 'Edge': 
+        alert("You've got the Edge!");
+        break;
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera': 
+        alert( 'Okay we support these browsers too' );
+        break;
+    default:
+        alert( 'We hope that this page looks ok!' );
+  }
+
+// Bai 2
+    var a = prompt('a?', '');
+    switch (a) {
+        case 0:
+        alert( 0 );
+        break;
+        case 1:
+        alert( 1 );
+        break;
+        case 2:
+        case 3:
+        alert( '2,3' );
+        break;
+    }
+  
