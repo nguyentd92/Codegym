f6cc495ab44f19331e93f33296b61dd751b0b8f5
unknown <nguyentd92@gmail.com>
2019-02-13 13:35:21 +0700

    findminvalue

---
+++ Module2/findminvalue/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function minOfArray($array) {
+            $min = $array[0];
+            $position = 0;
+            foreach ($array as $index => $number) {
+                if ($min > $array[$index]) {
+                    $min = $number;
+                    $position = $index;
+                };            
+            }
+
+            $result = array($min, $position);
+            return $result;
+        };
+    ?>
+    <form method="post">
+        <h3>Nhập dãy số nguyên cách nhau bởi khoảng trắng</h3>
+        <label>Mảng số nguyên: </label><input type="text" name="str"/>
+        <input type="submit" value="Find"/>
+    </form>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $str = $_POST["str"];
+
+            $regex = "/^[0-9 ]+$/";
+
+            if ( preg_match($regex, $str)) {
+                $array = explode(' ',$str);
+
+                $result = minOfArray($array);
+    
+                echo "Dãy số: ";
+                foreach ($array as $index => $value) {
+                    echo $value." ";
+                }
+                echo "<br/>Giá trị nhỏ nhất: $result[0] tại index $result[1]";
+            } else {
+                echo "Nhập sai";
+            }
+
+           
+
+        }
+    ?>
+</body>
+</html>
