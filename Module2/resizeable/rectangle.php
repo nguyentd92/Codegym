e71f6271fbca11705e64b214f616cfcf2b7672eb
unknown <nguyentd92@gmail.com>
2019-02-15 17:08:51 +0700

    update

---
+++ Module2/resizeable/rectangle.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once ('shape.php');
+class Rectangle extends Shape implements iResizeable
+{
+    public $width;
+    public $height;
+    public function __construct($name, $width, $height)
+    {
+        parent::__construct($name);
+        $this->width = $width;
+        $this->height = $height;
+    }
+    public function calculateArea(){
+        return $this->height * $this->width;
+    }
+    public function calculatePerimeter(){
+        return ($this->height + $this->width) * 2;
+    }
+
+    public function resize($percent){
+        $this->width *= sqrt($percent/100);
+        $this->height *= sqrt($percent/100);
+    }
+}
