a9a91b104f25d7338be2070c659324bea24c9eaa
unknown <nguyentd92@gmail.com>
2019-03-11 09:25:50 +0700

    restful

---
+++ laravel-restful-api/app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton(
+            CustomerRepository::class,
+            CustomerRepositoryImpl::class
+        );
+        $this->app->singleton(
+            CustomerService::class,
+            CustomerServiceImpl::class
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
